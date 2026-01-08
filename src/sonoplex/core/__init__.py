275519631a712234eb2cc3461d015a20c0b973b8
aquantumofdonuts <22332223+aquantumofdonuts@users.noreply.github.com>
2026-01-08 15:46:00 -0800

    refactor: create sonoplex.core.http module

--- src/sonoplex/core/__init__.py
+++ src/sonoplex/core/__init__.py
@@ -2,5 +2,10 @@
 
 from .config import Settings, settings, atomic_write_json
 from .logging import configure_logging
+from .http import HttpClient, http_client
 
-__all__ = ["Settings", "settings", "atomic_write_json", "configure_logging"]
+__all__ = [
+    "Settings", "settings", "atomic_write_json",
+    "configure_logging",
+    "HttpClient", "http_client",
+]
